cd711e08a356ad3b4e0a324473efd951da94ed6a
Amisha <amisharana960@gmail.com>
2025-04-19 11:35:34 +0530

    fixed

--- src/Componets/Nav.js
+++ src/Componets/Nav.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import image from "../Images/logo.jpg";
 import { Link } from "react-router-dom";
 
-
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,35 +9,21 @@ const Nav = () => {
     {
       name: "Home",
       link: "/",
-      //   icon: <ion-icon name="home-outline"></ion-icon>,
     },
-
-    // {
-    //   name: "Term & Condition",
-    //   link: "/termcondition",
-    //   icon: <ion-icon name="newspaper-outline"></ion-icon>,
-    // },
     {
       name: "About Us",
       link: "/about",
-      // icon: <ion-icon name="people-outline"></ion-icon>,
     },
     {
       name: "Contact",
       link: "/contact",
-      // icon: <ion-icon name="person-add-outline"></ion-icon>,
-    },
-    {
-      // name: "Login",
-      // link: "/login",
-      // icon: <ion-icon name="people-outline"></ion-icon>,
     },
   ];
 
   return (
     <div className="shadow-md w-full fixed top-0 left-0 bg-white z-10">
       <div className="flex items-center justify-between p-2">
-
+        {/* Logo */}
         <div className="flex items-center">
           <img
             src={image}
@@ -49,13 +34,14 @@ const Nav = () => {
           />
         </div>
 
+        {/* Desktop Links */}
         <div className="hidden md:flex flex-grow justify-center items-center space-x-6">
           <ul className="flex items-center space-x-6">
             {Links.map((link, index) => (
               <li key={index} className="text-xl">
                 <Link
                   to={link.link}
-                  className="flex items-center space-x-2 text-gray-800 hover:text-red-600 "
+                  className="flex items-center space-x-2 text-gray-800 hover:text-red-600"
                 >
                   {link.icon}
                   <span>{link.name}</span>
@@ -65,8 +51,8 @@ const Nav = () => {
           </ul>
         </div>
 
-
-        <div className="hidden md:flex items-center space-x-4 text-red-700 ">
+        {/* Social Icons - Desktop */}
+        <div className="hidden md:flex items-center space-x-4 text-red-700">
           <button style={{ fontSize: "28px" }}>
             <ion-icon name="logo-instagram"></ion-icon>
           </button>
@@ -78,6 +64,7 @@ const Nav = () => {
           </button>
         </div>
 
+        {/* Mobile Menu Button */}
         <div className="md:hidden flex items-center">
           <button onClick={() => setIsOpen(!isOpen)}>
             <ion-icon
@@ -88,11 +75,12 @@ const Nav = () => {
         </div>
       </div>
 
-
+      {/* Mobile Menu */}
       <div
         className={`md:hidden fixed inset-0 bg-white py-4 px-6 border-t border-gray-200 transition-transform duration-300 ease-in-out ${isOpen ? "transform translate-y-0" : "transform translate-y-full"
           }`}
       >
+        {/* Close Button */}
         <div className="flex justify-end mb-4">
           <button
             onClick={() => setIsOpen(false)}
@@ -102,14 +90,16 @@ const Nav = () => {
               name="close-outline"
               style={{ fontSize: "40px" }}
             ></ion-icon>
-
           </button>
         </div>
+
+        {/* Mobile Links */}
         <ul className="space-y-4 text-center">
           {Links.map((link, index) => (
             <li key={index} className="text-xl">
               <Link
                 to={link.link}
+                onClick={() => setIsOpen(false)} // 🔥 This closes the menu after click
                 className="flex items-center justify-center space-x-2 text-gray-800 hover:text-blue-600"
               >
                 {link.icon}
@@ -119,7 +109,8 @@ const Nav = () => {
           ))}
         </ul>
 
-        <div className="flex justify-center space-x-4 text-red-700 gap-5">
+        {/* Social Icons - Mobile */}
+        <div className="flex justify-center space-x-4 text-red-700 gap-5 mt-6">
           <button>
             <ion-icon name="logo-instagram"></ion-icon>
           </button>
